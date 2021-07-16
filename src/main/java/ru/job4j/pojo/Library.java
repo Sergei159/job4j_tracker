2a098bf0334fa8270d2ea4a784290946e5cf6579
55 <qars2010@yandex.ru>
2021-07-16 17:34:40 +0500

    Массивы и модели. [#174892 #178319]

--- src/main/java/ru/job4j/pojo/Library.java
+++ src/main/java/ru/job4j/pojo/Library.java
@@ -8,21 +8,21 @@ public class Library {
         Book nightWork = new Book("\"Night work\"", 430);
         Book[] books
                 = {airport, cleanCode, liveWithLighting, nightWork};
-        for (int i = 0; i < books.length; i++) {
-            System.out.println("Name: " + books[i].getName() + ". "
-                + "Number of pages: " + books[i].getNumber());
+        for (Book book : books) {
+            System.out.println("Name: " + book.getName() + ". "
+                    + "Number of pages: " + book.getNumber());
         }
         Book temp = books[0];
         books[0] = books[3];
         books[3] = temp;
         System.out.println("After changing");
-        for (int i = 0; i < books.length; i++) {
-            System.out.println("Name: " + books[i].getName() + ". "
-                    + "Number of pages: " + books[i].getNumber());
+        for (Book book : books) {
+            System.out.println("Name: " + book.getName() + ". "
+                    + "Number of pages: " + book.getNumber());
         }
         System.out.println();
         for (int i = 0; i < books.length; i++) {
-            if (books[i].getName().equals("\"Clean code\"")) {
+            if  ("\"Clean code\"".equals(books[i].getName())) {
                 System.out.println("Index of searched book is : " + i);
             }
         }
