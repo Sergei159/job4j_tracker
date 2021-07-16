8f49dba463c5d66c787cb9a4f1865244c13451fd
55 <qars2010@yandex.ru>
2021-07-16 11:37:49 +0500

    Date. Отображение даты. [#310017]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -1,11 +1,23 @@
 package ru.job4j.tracker;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Item {
+
     private int id;
+
     private String name;
 
-   public Item() {
-   }
+    private LocalDateTime created = LocalDateTime.now();
+
+    private DateTimeFormatter formatter
+            = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
+
+    private String currentDateTimeFormat = created.format(formatter);
+
+    public Item() {
+    }
 
     public Item(String name) {
        this.name = name;
@@ -15,4 +27,13 @@ public class Item {
        this.id = id;
        this.name = name;
     }
+
+    public LocalDateTime getCreated() {
+        return created;
+    }
+
+    public String getCurrentDateTimeFormat() {
+        return currentDateTimeFormat;
+    }
+
 }
