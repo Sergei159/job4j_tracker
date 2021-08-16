a8988b5e64258e00913c2cb52df65131ebf7a6fc
55 <qars2010@yandex.ru>
2021-08-16 11:49:21 +0500

    Тест на ValidateInput [#311887 #189312]

--- src/test/java/ru/job4j/tracker/ValidateInputTest.java
+++ src/test/java/ru/job4j/tracker/ValidateInputTest.java
@@ -17,4 +17,41 @@ public class ValidateInputTest {
         int selected = input.askInt("Enter menu:");
         assertThat(selected, is(1));
     }
+
+    @Test
+    public void whenRightInput() {
+        Output out = new StubOutput();
+        Input in = new StubInput(
+                new String[] {"1"}
+        );
+        ValidateInput input = new ValidateInput(out, in);
+        int selected = input.askInt("Enter menu:");
+        assertThat(selected, is(1));
+    }
+
+    @Test
+    public void whenMultipleRightInput() {
+        Output out = new StubOutput();
+        Input in = new StubInput(
+                new String[] {"0", "1", "2"}
+        );
+        ValidateInput input = new ValidateInput(out, in);
+        int selected1 = input.askInt("Enter menu:");
+        assertThat(selected1, is(0));
+        int selected2 = input.askInt("Enter menu:");
+        assertThat(selected2, is(1));
+        int selected3 = input.askInt("Enter menu:");
+        assertThat(selected2, is(2));
+    }
+
+    @Test
+    public void whenIsNegativeInput() {
+        Output out = new StubOutput();
+        Input in = new StubInput(
+                new String[] {"-1"}
+        );
+        ValidateInput input = new ValidateInput(out, in);
+        int selected = input.askInt("Enter menu:");
+        assertThat(selected, is(-1));
+    }
 }
