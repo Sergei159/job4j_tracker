0a4034c32ead516c274b483999d357ada1b7b197
55 <qars2010@yandex.ru>
2021-09-17 18:20:17 +0500

    Фильтрация учеников [#110058]

--- src/main/java/ru/job4j/search/PhoneDictionary.java
+++ src/main/java/ru/job4j/search/PhoneDictionary.java
@@ -10,18 +10,6 @@ public class PhoneDictionary {
         this.persons.add(person);
     }
 
-//    public ArrayList<Person> find(String key) {
-//        ArrayList<Person> result = new ArrayList<>();
-//        for (Person person : persons) {
-//            if (person.getName().contains(key)
-//                    || person.getSurname().contains(key)
-//                    || person.getPhone().contains(key)
-//                    || person.getAddress().contains(key)) {
-//                result.add(person);
-//            }
-//        }
-//        return result;
-//    }
     public ArrayList<Person> find(String key) {
         Predicate<Person> hasName = (p) -> p.getName().contains(key);
         Predicate<Person> hasSurname = (p) -> p.getSurname().contains(key);
