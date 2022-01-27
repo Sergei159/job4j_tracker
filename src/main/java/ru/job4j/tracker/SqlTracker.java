2149616ac57a48aa43ab099e5de30ffd33db4e39
sergei159 <89082766060QAzQAz>
2022-01-27 11:22:12 +0500

    Трекер SQL [#1734]

--- src/main/java/ru/job4j/tracker/SqlTracker.java
+++ src/main/java/ru/job4j/tracker/SqlTracker.java
@@ -1,8 +1,5 @@
 package ru.job4j.tracker;
 
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
 import java.io.InputStream;
 import java.sql.*;
 import java.util.ArrayList;
