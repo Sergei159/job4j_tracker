a77caba0f1768bd53ac7ad181107a1a053b9f70b
sergei159 <89082766060QAzQAz>
2022-01-28 12:08:25 +0500

    Трекер SQL [#1734 #255815]

--- src/test/java/ru/job4j/tracker/SqlTrackerTest.java
+++ src/test/java/ru/job4j/tracker/SqlTrackerTest.java
@@ -4,10 +4,7 @@ import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
-import ru.job4j.tracker.Item;
 
-import java.io.FileReader;
-import java.io.IOException;
 import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -17,7 +14,6 @@ import java.util.List;
 import java.util.Properties;
 
 import static org.hamcrest.Matchers.is;
-import static org.hamcrest.Matchers.nullValue;
 import static org.junit.Assert.assertThat;
 
 public class SqlTrackerTest {
@@ -62,52 +58,41 @@ public class SqlTrackerTest {
     }
 
     @Test
-    public void whenSQLFindByName() {
+    public void whenFindByName() {
         SqlTracker tracker = new SqlTracker(connection);
-        Item item1 = new Item("item");
-        Item item2 = new Item("item");
-        tracker.add(item1);
-        tracker.add(item2);
+        Item item1 = tracker.add(new Item("item"));
+        Item item2 = tracker.add(new Item("item"));
         List<Item> result = tracker.findByName("item");
-        assertThat(result.size(), is(2));
+        assertThat(result, is(List.of(item1, item2)));
     }
 
     @Test
-    public void whenSQLFindAll() {
+    public void whenFindAll() {
         SqlTracker tracker = new SqlTracker(connection);
-        Item item1 = new Item("item");
-        Item item2 = new Item("item2");
-        Item item3 = new Item("item3");
-        tracker.add(item1);
-        tracker.add(item2);
-        tracker.add(item3);
+        Item item1 = tracker.add(new Item("item"));
+        Item item2 = tracker.add(new Item("item2"));
+        Item item3 = tracker.add(new Item("item3"));
         List<Item> result = tracker.findAll();
-        assertThat(result.size(), is(3));
+        assertThat(result, is(List.of(item1, item2, item3)));
     }
 
     @Test
-    public void whenSQLDelete() {
+    public void whenDelete() {
         SqlTracker tracker = new SqlTracker(connection);
-        Item item1 = new Item("item");
-        Item item2 = new Item("item2");
-        Item item3 = new Item("item3");
-        tracker.add(item1);
-        tracker.add(item2);
-        tracker.add(item3);
+        Item item1 = tracker.add(new Item("item"));
+        Item item2 = tracker.add(new Item("item2"));
+        Item item3 = tracker.add(new Item("item3"));
         tracker.delete(item1.getId());
         List<Item> result = tracker.findAll();
-        assertThat(result.size(), is(2));
+        assertThat(result, is(List.of(item2, item3)));
     }
 
     @Test
     public void whenSQLReplace() {
         SqlTracker tracker = new SqlTracker(connection);
-        Item item1 = new Item("item");
-        Item item2 = new Item("item2");
-        Item item3 = new Item("item3");
-        tracker.add(item1);
-        tracker.add(item2);
-        tracker.add(item3);
+        Item item1 = tracker.add(new Item("item"));
+        Item item2 = tracker.add(new Item("item2"));
+        Item item3 = tracker.add(new Item("item3"));
         tracker.replace(item1.getId(), item3);
         assertThat(tracker.findById(item1.getId()).getName(), is(item3.getName()));
     }
