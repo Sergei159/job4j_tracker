fc03ea31e1ce6f824b2eea94023e57d4f6737105
Sergei159 <qars2010@yandex.ru>
2022-04-21 01:07:35 +0500

    Интеграционные тесты с Hqldb [#4746]

--- src/main/java/ru/job4j/tracker/HbmTracker.java
+++ src/main/java/ru/job4j/tracker/HbmTracker.java
@@ -2,12 +2,14 @@ package ru.job4j.tracker;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.query.Query;
 
 import java.util.List;
+import java.util.function.Function;
 
 public class HbmTracker implements Store, AutoCloseable {
 
@@ -16,69 +18,73 @@ public class HbmTracker implements Store, AutoCloseable {
     private final SessionFactory sf = new MetadataSources(registry)
             .buildMetadata().buildSessionFactory();
 
+    /**
+     * Приватный метод, чтобы избежать дублирование кода
+     * открывает сессию, начинает транзакцию
+     * после выполнения входной функции закрывает сессию
+     * @param command
+     * @param <T>
+     * @return
+     */
+    private <T> T transaction(final Function<Session, T> command) {
+        final Session session = sf.openSession();
+        final Transaction transaction = session.beginTransaction();
+        try {
+            T result = command.apply(session);
+            transaction.commit();
+            return result;
+        } catch (final Exception e) {
+            session.getTransaction().rollback();
+            throw e;
+        } finally {
+            session.close();
+        }
+    }
+
     @Override
     public Item add(Item item) {
-        Session session = sf.openSession();
-        session.beginTransaction();
-        session.save(item);
-        session.getTransaction().commit();
-        session.close();
+        transaction(session -> session.save(item));
         return item;
     }
 
     @Override
     public boolean replace(int id, Item item) {
-        Session session = sf.openSession();
-        session.beginTransaction();
         item.setId(id);
-        session.update(item);
-        session.getTransaction().commit();
-        session.close();
+        transaction(session ->  {
+            session.update(item);
+            return new Object();
+        });
         return item.equals(findById(id));
     }
 
     @Override
     public boolean delete(int id) {
-        Session session = sf.openSession();
-        session.beginTransaction();
         Item item = new Item(null);
         item.setId(id);
-        session.delete(item);
-        session.getTransaction().commit();
-        session.close();
+        transaction(session -> {
+            session.delete(item);
+            return new Object();
+        });
         return true;
     }
 
     @Override
     public List<Item> findAll() {
-        Session session = sf.openSession();
-        session.beginTransaction();
-        List result = session.createQuery("from ru.job4j.tracker.Item").list();
-        session.getTransaction().commit();
-        session.close();
-        return result;
+        return transaction(session -> session
+                .createQuery("from ru.job4j.tracker.Item")
+                .list());
     }
 
     @Override
     public List<Item> findByName(String key) {
-        Session session = sf.openSession();
-        session.beginTransaction();
-        Query query = session.createQuery("from ru.job4j.tracker.Item where name = :keyName");
-        query.setParameter("keyName", key);
-        List<Item> result = query.list();
-        session.getTransaction().commit();
-        session.close();
-        return result;
+        return transaction(session -> session.createQuery(
+                "from ru.job4j.tracker.Item where name = :keyName")
+                .setParameter("keyName", key).list());
     }
 
     @Override
     public Item findById(int id) {
-        Session session = sf.openSession();
-        session.beginTransaction();
-        Item result = session.get(Item.class, id);
-        session.getTransaction().commit();
-        session.close();
-        return result;
+        return transaction(session -> session.get(Item.class, id));
     }
 
     @Override
